1be676b2a1c0942c2eb424e6ee4ef626d763bf35
Ying Shaodong <helloysd@gmail.com>
2024-08-22 16:11:31 +0800

    feat: adding metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,12 @@ import {CreateInvoice} from '@/app/ui/invoices/buttons'
 import {lusitana} from '@/app/ui/fonts'
 import {Suspense} from 'react'
 import {InvoicesTableSkeleton} from '@/app/ui/skeletons'
-import { fetchInvoicesPages } from '@/app/lib/data';
+import {fetchInvoicesPages} from '@/app/lib/data'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
@@ -18,7 +23,7 @@ export default async function Page({
   const query = searchParams?.query || ''
   const currentPage = Number(searchParams?.page) || 1
 
-  const totalPages = await fetchInvoicesPages(query);
+  const totalPages = await fetchInvoicesPages(query)
 
   return (
     <div className="w-full">
